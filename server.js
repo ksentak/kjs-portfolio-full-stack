b5a57edc3d3339bfa4cb0a8bb14971e1b0c3234f
Keaton Sentak <k.sentak@outlook.com>
2020-08-16 16:16:37 -0400

    add .env and upload config

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const nodemailer = require('./routes/nodemailer');
-
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	);
-	next();
-});
+require('dotenv').config();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('client/build'));
-
+// Nodemailer route
 app.use('/', nodemailer);
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-});
+// Heroku production
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve up production assets
+	app.use(express.static('client/build'));
+
+	// Express serves up index.html file if it doesn't recognize route
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+	});
+}
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
