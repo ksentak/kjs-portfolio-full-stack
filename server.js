4c47441964039db9e86871eb98280e75ca9250f4
Keaton Sentak <k.sentak@outlook.com>
2020-08-15 22:00:42 -0400

    modify server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 5000;
 
-app.get('/', (req, res) => {
-	res.send('App is running express');
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 });
 
 app.listen(port, () => {
