9890a80c00c18e446d76a0110055f0a612c5eec8
Keaton Sentak <k.sentak@outlook.com>
2020-08-16 00:08:02 -0400

    modify navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import '../assets/css/Navbar.css';
+
+class Navbar extends Component {
+	render() {
+		return (
+			<div className='Navbar'>
+				<nav className='navbar navbar-expand-sm'>
+					<div className='container'>
+						<button
+							className='navbar-toggler ml-auto'
+							type='button'
+							data-toggle='collapse'
+							data-target='#navbarMobile'
+							aria-controls='navbarMobile'
+							aria-expanded='false'
+							aria-label='Toggle navigation'
+						>
+							<span className='navbar-toggler-icon' />
+						</button>
+						<div className='collapse navbar-collapse' id='navbarMobile'>
+							<ul id='menu-primary' className='navbar-nav ml-auto'>
+								{/* Home */}
+								<li data-toggle='collapse' data-target='.navbar-collapse.show'>
+									<NavLink exact to='/' activeClassName='active' className='nav-Link'>
+										Home
+									</NavLink>
+								</li>
+								{/* About */}
+								<li data-toggle='collapse' data-target='.navbar-collapse.show'>
+									<NavLink
+										exact
+										to='/about'
+										activeClassName='active'
+										className='nav-Link'
+									>
+										About
+									</NavLink>
+								</li>
+								{/* Portfolio */}
+								<li data-toggle='collapse' data-target='.navbar-collapse.show'>
+									<NavLink
+										exact
+										to='/portfolio'
+										activeClassName='active'
+										className='nav-Link'
+									>
+										Portfolio
+									</NavLink>
+								</li>
+								{/* Resume */}
+								{/* <li>
+                  <NavLink
+                    exact
+                    to='/resume'
+                    activeClassName='active'
+                    className='nav-Link'
+                  >
+                    Resume
+                  </NavLink>
+                </li> */}
+								{/* Contact */}
+								<li data-toggle='collapse' data-target='.navbar-collapse.show'>
+									<NavLink
+										exact
+										to='/contact'
+										activeClassName='active'
+										className='nav-Link'
+									>
+										Contact
+									</NavLink>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+		);
+	}
+}
+
+export default Navbar;
