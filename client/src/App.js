46ca0b9e278149292fcf6c81f800d13ec4186639
Keaton Sentak <k.sentak@outlook.com>
2020-08-15 22:27:02 -0400

    add code to app and index files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+// Components
+import Navbar from './components/Navbar';
+// CSS
+import './assets/css/main.css';
 
-function App() {
-	return <div className='App'></div>;
-}
+const App = () => {
+	return (
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/about' component={About} />
+				<Route exact path='/portfolio' component={Portfolio} />
+				<Route exact path='/contact' component={Contact} />
+				<Route component={Error} />
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
