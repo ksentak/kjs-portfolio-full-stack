b3b6094e5ffd4a7d3c5c84cf641b3d54f7b15420
Keaton Sentak <k.sentak@outlook.com>
2020-08-16 23:38:17 -0400

    add build folder to repo for access on gcloud

---
+++ client/build/precache-manifest.60273b0dcc98e6c753dc625603b632ea.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4491df410231e94642a3d228c77977",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a493c4f1cc4e73e79fad",
+    "url": "/static/css/2.3a221eca.chunk.css"
+  },
+  {
+    "revision": "56a6f49999d06e11146c",
+    "url": "/static/css/main.b19d2e0b.chunk.css"
+  },
+  {
+    "revision": "a493c4f1cc4e73e79fad",
+    "url": "/static/js/2.8a383803.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.8a383803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a6f49999d06e11146c",
+    "url": "/static/js/main.da5d2050.chunk.js"
+  },
+  {
+    "revision": "0d6274610ebdb7aa55bf",
+    "url": "/static/js/runtime-main.6c0a6773.js"
+  },
+  {
+    "revision": "bfdb0cd82a8ac27eb977bad8ddd9f774",
+    "url": "/static/media/GoogleBooks.bfdb0cd8.png"
+  },
+  {
+    "revision": "faf98f2a51e3803f4fb9bd7028218a71",
+    "url": "/static/media/Keaton_Sentak_Resume.faf98f2a.pdf"
+  },
+  {
+    "revision": "2084e4b72ea5654e918b04f0e5f5b5dc",
+    "url": "/static/media/liberty-beans.2084e4b7.png"
+  },
+  {
+    "revision": "35cfd0d2ff465dbaece105b0a2c32fdf",
+    "url": "/static/media/omega.35cfd0d2.png"
+  },
+  {
+    "revision": "a04b403d49560a9e865f8183cc1aab42",
+    "url": "/static/media/profile-headshot.a04b403d.jpg"
+  },
+  {
+    "revision": "7d7e15d1c0b14d3473736cddce16a5c4",
+    "url": "/static/media/react-auth.7d7e15d1.png"
+  },
+  {
+    "revision": "ff86580799f2ec28359242f0df55a0b8",
+    "url": "/static/media/react-colors.ff865807.png"
+  },
+  {
+    "revision": "2335c3e5f6535afafc150adb3f292b1b",
+    "url": "/static/media/time_machine.2335c3e5.png"
+  }
+]);
