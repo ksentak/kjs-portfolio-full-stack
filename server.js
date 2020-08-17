0d26323a849444317ac13961981bf058abb11137
Keaton Sentak <k.sentak@outlook.com>
2020-08-16 22:39:03 -0400

    instruct gcloud to use build files in server.js

--- server.js
+++ server.js
@@ -14,8 +14,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Nodemailer route
 app.use('/', nodemailer);
 
+// Production variable
+const liveSite = 1;
+
 // Heroku production
 if (process.env.NODE_ENV === 'production') {
+	// Express will serve up production assets
+	app.use(express.static('client/build'));
+
+	// Express serves up index.html file if it doesn't recognize route
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+	});
+}
+
+// Gcloud production
+if (liveSite == 1) {
 	// Express will serve up production assets
 	app.use(express.static('client/build'));
 
